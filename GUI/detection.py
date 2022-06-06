f9cb53d112eca226c76a09f8d7a174ed25d9f9fd
Jakub Krško <jakubkrs@outlook.sk>
2022-06-06 20:42:38 +0200

    Improved detection

--- GUI/detection.py
+++ GUI/detection.py
@@ -1,3 +1,5 @@
+#!/home/pi/miniforge3/bin/python3
+
 from PyQt import PyQtApp
 from PyQt5.QtGui import QPixmap
 from PyQt5 import QtGui
@@ -6,6 +8,7 @@ import threading
 import cv2
 import torch
 import pandas as pd
+from time import time
 
 img = None
 
@@ -22,15 +25,21 @@ def show_stream():
     while webcam.isOpened():
         if app.window.checkbox1.checkState() == 2: #fix issue with checkbox
             ret, img = webcam.read()
-            app.window.label1.setPixmap(opencv_to_image(img))
-            app.window.label1.setScaledContents(True)
+            cv2.imshow("Camera feed", img)
+            cv2.waitKey(1)
+            #app.window.label1.setPixmap(opencv_to_image(img))
+            #app.window.label1.setScaledContents(True)
+        else:
+            cv2.destroyAllWindows()
+            webcam.release() #releases, but can't be used next time, because can't get back in the loop
 
 def detect(Img) -> None: #TODO: separate this into a thread
     global img
     img = Img.copy()
     app.window.label1.setText("Detecting...")
-    model = torch.hub.load('ultralytics/yolov5', 'custom', path='best.pt')
-    result = model(img, size=3072)
+    start = time()   
+    #model = torch.hub.load("/home/pi/Desktop/yolov5", "custom", path="/home/pi/Desktop/realizacia_NN_na_Rasbperry_Pi/GUI/best.pt", source="local") #use local repository
+    result = model(img, size=2048)
     print(result.pandas().xyxy)
     # print(len(result.pandas().xyxy[0]))
     for i in range(len(result.pandas().xyxy[0])):
@@ -39,12 +48,14 @@ def detect(Img) -> None: #TODO: separate this into a thread
         cv2.putText(img, str(round(result.pandas().xyxy[0]["confidence"][i],3)), (xm, ym), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 0, 255), 5)
         cv2.rectangle(img, (xm, ym), (xM, yM), (0,0,255), 4)
     show_frame_in_display(opencv_to_image(img))
+    app.window.label2.setText(f"Detection time: {round(time()-start,1)} s")
 
 def loadImage():
     #open file open dialog
     fileName, _ = QtWidgets.QFileDialog.getOpenFileName(None, "Open Image", "", "Image Files (*.jpg *.png *.bmp)")
     if fileName:
         img = cv2.imread(fileName)
+        print("Detection started.")
         img = detect(img)
         #add thread for detection here
 
@@ -54,19 +65,25 @@ def saveImage():
         cv2.imwrite(fileName, img)
 
 def snapImage():
-    # add this functionality with pi camera
-    pass
+    app.window.checkbox1.setChecked(False)
+    cam = cv2.VideoCapture(0)
+    ret, img = cam.read()
+    img = detect(img)
+    
 
 def main():
-    stream = threading.Thread(target=show_stream)
-    # stream.daemon = True
-    # stream.start()
+#     stream = threading.Thread(target=show_stream)
+#     stream.daemon = True
+#     stream.start()
     app.window.button1.clicked.connect(loadImage)
     app.window.button2.clicked.connect(saveImage)
     app.window.button3.clicked.connect(snapImage)
     app.window.button4.clicked.connect(app.window.close)
+    app.window.label2.setText("")
+    print("Model initialized.")
     app.execute()
 
 if __name__ == "__main__":
-    app = PyQtApp("GUI/layout.ui")
-    main()
+    model = torch.hub.load("/home/pi/Desktop/yolov5", "custom", path="/home/pi/Desktop/realizacia_NN_na_Rasbperry_Pi/GUI/best.pt", source="local") #use local repository
+    app = PyQtApp("layout.ui")            
+    main()        
